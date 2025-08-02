579cbc4756b4e0f570ea8847d7d9dd6b2b2625c8
rogerio.silva <samanthadmedeiros37@gmail.com>
2025-08-02 16:29:26 -0300

    📖 Main file

---
+++ HealthDiary/src/main/java/com/sammid37/HealthDiary/HealthDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.sammid37.HealthDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDiaryApplication.class, args);
+	}
+
+}
