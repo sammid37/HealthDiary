9657d287876391d7d58755e381ec8c87f682104f
sammid37 <samanthadmedeiros37@gmail.com>
2025-08-21 17:40:23 -0300

    Adiciona projeto react utilizando Vite

---
+++ HealthDiary/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <button class="btn btn-primary">One</button>
+
+      <button class="btn btn-secondary">Two</button>
+      
+      <button class="btn btn-accent btn-outline">Three</button>
+    </>
+  )
+}
+
+export default App
